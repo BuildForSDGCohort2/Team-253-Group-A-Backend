f9c18b69703bb8f6030bfd47b0c00a382684fbfb
khammami <khammami@gmail.com>
2020-09-30 19:48:22 +0100

    Fix bind port timeout heroku

--- ai_part/model_api/docker_files/web_api_cleanout.py
+++ ai_part/model_api/docker_files/web_api_cleanout.py
@@ -61,4 +61,5 @@ def process_score_image_request():
     return jsonify(response)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
