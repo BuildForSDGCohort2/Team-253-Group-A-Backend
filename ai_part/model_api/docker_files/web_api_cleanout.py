68b296cf7dd362a4f97a213c211d60a6c336fd72
khammami <khammami@gmail.com>
2020-09-30 14:24:41 +0100

    Restore old port

--- ai_part/model_api/docker_files/web_api_cleanout.py
+++ ai_part/model_api/docker_files/web_api_cleanout.py
@@ -61,5 +61,4 @@ def process_score_image_request():
     return jsonify(response)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 80))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
