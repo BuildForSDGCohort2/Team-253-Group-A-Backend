cb142cbc4b37bffe581e9075ebd1d5b6d8e7d793
khammami <khammami@gmail.com>
2020-09-30 19:58:44 +0100

    Revert "Fix bind port timeout heroku"
    This reverts commit f9c18b69703bb8f6030bfd47b0c00a382684fbfb.

--- ai_part/model_api/docker_files/web_api_cleanout.py
+++ ai_part/model_api/docker_files/web_api_cleanout.py
@@ -61,5 +61,4 @@ def process_score_image_request():
     return jsonify(response)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=5000, debug=True)
